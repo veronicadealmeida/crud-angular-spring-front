d4098ee2e6e79916367226d10d2b55cf1c9f037b
Veronica <almeida.vrc@gmail.com>
2023-09-14 20:28:22 -0300

    Cria metodo put

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -25,6 +25,22 @@ export class CoursesService {
   }
 
   save(record: Partial<Course>) {
+    if (record._id) {
+      console.log('update');
+      return this.update(record);
+    }
+    console.log('create');
+
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>) {
     return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
+
+  private update(record: Partial<Course>) {
+    return this.httpClient
+      .put<Course>(`${this.API}/${record._id}`, record)
+      .pipe(first());
   }
 }
