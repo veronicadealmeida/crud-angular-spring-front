6bd5f617bb1c4ad4c69fc54fef05a087e2c16f4b
Veronica <almeida.vrc@gmail.com>
2023-09-14 14:33:20 -0300

    refactor do component courses

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -7,9 +7,10 @@ import { SharedModule } from '../shared/shared.module';
 import { CourseFormComponent } from './course-form/course-form.component';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
+import { CoursesListComponent } from './courses-list/courses-list.component';
 
 @NgModule({
-  declarations: [CoursesComponent, CourseFormComponent],
+  declarations: [CoursesComponent, CourseFormComponent, CoursesListComponent],
   imports: [
     CommonModule,
     CoursesRoutingModule,
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -14,7 +14,6 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>;
-  displayedColumns = ['name', 'category', 'actions'];
 
   constructor(
     private coursesService: CoursesService,
