fa04d53660beac7391f9245c82ffd0d051689fe5
Veronica <almeida.vrc@gmail.com>
2023-09-14 13:33:54 -0300

    Adiciona ação de salvar

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -15,7 +16,8 @@ export class CourseFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location
   ) {
     this.form = this.formBuilder.group({
       name: [null],
@@ -26,18 +28,23 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit() {
     this.service.save(this.form.value).subscribe(
-      (result) => console.log(result),
+      (result) => this.onSuccess(),
       (error: any) => {
         this.onError();
       }
     );
+    this.onCancel();
   }
 
   onCancel() {
-    console.log('onCancel');
+    this.location.back();
   }
 
   private onError() {
     this.snackBar.open('Erro ao salvar curso', '', { duration: 5000 });
+  }
+
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso', '', { duration: 5000 });
   }
 }
